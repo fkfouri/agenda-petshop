af5f0ace2dc9a84f490a8608d4b5e05c88cf4327
Fabio Kfouri <fabio.kfouri@embraer.net.br>
2020-01-24 08:36:21 -0300

    Estudo de transição de uma API de um modo REST para GraphQL

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const customExpress = require('./config/custom-express')
+const { GraphQLServer } = require('graphql-yoga')
+//const customExpress = require('./config/custom-express')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
+const Operacoes = require('./infraestrutura/operations')
 
-const app = customExpress()
+
+//const app = customExpress()
 
 conexao.connect(erro => {
   if (erro) {
@@ -14,6 +17,33 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
+//app.listen(4000, () => {
+//  console.log('Servidor rodando na porta 4000')
+//})
+
+const Clientes = new Operacoes('cliente')
+const resolvers= {
+  Query : {
+    status: () => "servidor rodando!",
+    clientes: () => Clientes.lista(),
+    cliente: (root, {id}) => Clientes.buscaPorId( id )
+    //status : "resolver funcionando"
+  },
+  Mutation:{
+    //adicionarCliente : (root, params) => ({
+    //  id: 3,
+    //  nome: params.nome,
+    //  cpf: params.cpf
+    //}) 
+
+    adicionarCliente : (root, params) => Clientes.adiciona(params)
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers: resolvers,
+  typeDefs : './schema.graphql'
+
 })
+
+servidor.start(() => console.log('servidor ouvindo'))
